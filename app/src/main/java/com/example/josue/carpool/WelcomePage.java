4c496e4f02aeca948ebd800ab95e18a9aab8fd1d
josuecb <josue.carbonel@live.lagcc.cuny.edu>
2016-10-09 10:25:23 -0400

    Improvements
    Improvements

--- app/src/main/java/com/example/josue/carpool/WelcomePage.java
+++ app/src/main/java/com/example/josue/carpool/WelcomePage.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
 
 /**
  * Created by Josue on 10/8/2016.
@@ -17,6 +20,7 @@ public class WelcomePage extends AppCompatActivity implements View.OnClickListen
     Button sign_in_btn, register_btn;
     LocalStorage localStorage;
     Context context;
+    EditText phone, password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +28,8 @@ public class WelcomePage extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.welcome_layout);
         sign_in_btn = (Button) findViewById(R.id.sign_in_btn);
         register_btn = (Button) findViewById(R.id.register_btn);
+        phone = (EditText) findViewById(R.id.user_phone_content_id);
+        password = (EditText) findViewById(R.id.user_password_content_id);
         localStorage = new LocalStorage(CarPool.LOCAL_STORAGE_CRED);
         context = this;
 
@@ -39,6 +45,7 @@ public class WelcomePage extends AppCompatActivity implements View.OnClickListen
         switch (v.getId()) {
             case R.id.sign_in_btn:
                 Log.e("sign in ", "clicked");
+                onSignInBtn();
                 break;
             case R.id.register_btn:
                 Log.e("register", "clicked");
@@ -53,7 +60,32 @@ public class WelcomePage extends AppCompatActivity implements View.OnClickListen
      * Store credentials in local data storage
      */
     public void onSignInBtn() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UserPost data = ApiClient.locationApi.sendRequest(
+                            new UserCredentialsLogin(Long.parseLong(phone.getText().toString()),
+                                    password.getText().toString())).execute().body();
+                    if (!data.authDetails.email.isEmpty()) {
+                        localStorage.save(CarPool.USER_NAME_LABEL, data.userDetails.fullName, context);
+                        localStorage.save(CarPool.USER_EMAIL_LABEL, data.authDetails.email, context);
+                        localStorage.save(CarPool.USER_PHONE_LABEL, data.authDetails.phone, context);
+                        localStorage.save(CarPool.USER_STATEID_LABEL, data.userDetails.stateId, context);
+                        localStorage.save(CarPool.USER_ID, String.valueOf(data.userDetails.id), context);
+                        localStorage.save(CarPool.USER_PASS_LABEL, data.accessTokenDetails.token, context);
+                        localStorage.save(CarPool.USER_POINTS_LABEL, "100", context);
+                        Log.i("data", data.authDetails.email);
 
+                        Intent goToMap = new Intent(context, MapsActivity.class);
+                        startActivity(goToMap);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
     }
 
     public void onRegisterBtn() {
